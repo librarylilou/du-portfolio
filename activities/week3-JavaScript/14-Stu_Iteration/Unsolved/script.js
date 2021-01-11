2f2a471eea74f3563fe058fc415114bda3f78dc9
Valeria Rodriguez <val.lilou.rod@gmail.com>
2021-01-10 23:21:41 -0700

    Homework week 2

---
+++ activities/week3-JavaScript/14-Stu_Iteration/Unsolved/script.js
@@ -0,0 +1,11 @@
+//WRITE YOUR CODE BELOW
+var students= ["val", "william" , "john", "greg"]
+
+for(var i=0; i<students.length; i++){
+    console.log ("Great to see you," + students[i]);
+}
+
+while (i< 2){
+    console.log("1st and 2nd:");
+    i++;
+}
---
+++ activities/week3-JavaScript/16-Stu_Functions/Unsolved/script.js
@@ -0,0 +1,7 @@
+// Write Your Code Below
+var food = ["olives", "pepporoni", "garlic", "peppers", "garlic"];
+
+while (food[i]== i++)
+{
+    console.log("They are equal in type and value");
+}
